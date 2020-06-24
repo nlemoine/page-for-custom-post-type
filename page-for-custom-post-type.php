61c75df02cb03ada9c7e91205ba006f4907f8086
Nicolas Lemoine <nico.lemoine@gmail.com>
2020-06-24 15:20:29 +0200

    Up

--- page-for-custom-post-type.php
+++ page-for-custom-post-type.php
@@ -622,8 +622,19 @@ function get_custom_post_type_for_page($post_id) {
     $pfcpt = Page_For_Custom_Post_Type::get_instance();
     $page_ids = $pfcpt->get_page_ids();
 
+    $post_id = (int) $post_id;
+
     if(!in_array($post_id, $page_ids, true)) {
         return false;
     }
     return array_search($post_id, $page_ids, true);
+}
+function get_page_for_custom_post_type($post_type) {
+    $pfcpt = Page_For_Custom_Post_Type::get_instance();
+    $page_ids = $pfcpt->get_page_ids();
+    return $page_ids[$post_type] ?? false;
+}
+function get_page_for_custom_post_type_link($post_type) {
+    $page_id = get_page_for_custom_post_type($post_type);
+    return $page_id ? get_permalink($page_id) : false;
 }

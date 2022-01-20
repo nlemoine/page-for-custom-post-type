3ceca654d257186e0ec5df29ec29a2de66758068
Nicolas Lemoine <nico.lemoine@gmail.com>
2022-01-20 14:54:29 +0100

    up

--- src/integrations/wordpress-seo.php
+++ src/integrations/wordpress-seo.php
@@ -28,7 +28,7 @@ function fix_post_breadcrumbs(array $indexables, $context)
     $pfcpt = Plugin::get_instance();
 
     // Check if current post type has a page for custom post type
-    $page_for_post_type_id = $pfcpt->get_page_id_from_post_type($current_post_type, true);
+    $page_for_post_type_id = $pfcpt->get_page_id_from_post_type($current_post_type, function_exists('PLL'));
     if(!$page_for_post_type_id) {
         return $indexables;
     }
@@ -83,7 +83,7 @@ function fix_taxonomy_breadcrumbs(array $indexables, $context)
     $pfcpt = Plugin::get_instance();
 
     // Check if current post type has a page for custom post type
-    $page_for_post_type_id = $pfcpt->get_page_id_from_post_type($current_post_type, false);
+    $page_for_post_type_id = $pfcpt->get_page_id_from_post_type($current_post_type, function_exists('PLL'));
     if(!$page_for_post_type_id) {
         return $indexables;
     }
